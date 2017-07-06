259e42c0ef91ad1dde3c13c0aed21985a73be1e4
Sebastian Rettenberger <rettenbs@in.tum.de>
2017-07-06 15:50:52 +0200

    Try pkg-config for hdf5 and check API version

--- libs/hdf5.py
+++ libs/hdf5.py
@@ -38,9 +38,27 @@ import utils.checks
 import utils.compiler
 import utils.pkgconfig
 
-def find(env, required=True, parallel=False, hl=True):
+__hdf5_api_check = """
+#include <hdf5.h>
+
+#ifdef H5_USE_%s_API
+int main(int argc, char* argv) {
+#endif
+	return 0;
+}
+"""
+
+def CheckAPIVersion(context, api):
+	context.Message('Checking for HDF5 v%s API... ' % (api,))
+	ret = context.TryCompile(__hdf5_api_check % (api,), '.c')
+	context.Result(ret)
+
+	return ret
+
+def find(env, required=True, parallel=False, hl=True, api=18):
 	conf = env.Configure()
 	utils.checks.addDefaultTests(conf)
+	conf.AddTests({'CheckAPIVersion': CheckAPIVersion})
 
 	# Find h5cc or h5pcc
 	h5ccs = ['h5cc', 'h5pcc']
@@ -53,23 +71,32 @@ def find(env, required=True, parallel=False, hl=True):
 		if h5cc:
 			break
 
-	if not h5cc:
-		if required:
-			utils.checks.error('Cannot find h5cc or h5pcc: Make sure the path to the HDF5 library is correct')
-			env.Exit(1)
-		else:
-			conf.Finish()
-			return False
+	if h5cc:
+		# Parse the output from the h5cc compiler wrapper
+		def parse_func(env, cmd):
+			# remove the compiler
+			cmd = cmd.partition(' ')[2]
+			# remove unknown arguments
+			cmd = utils.compiler.removeUnknownOptions(cmd)
+			return env.ParseFlags(cmd)
+		flags = env.ParseConfig([h5cc, '-show', '-shlib'], parse_func)
+	else:
+		# Try pkg-config
+		hdf5s = ['hdf5_hl', 'hdf5_hl_parallel']
+		if parallel:
+			hdf5s[0], hdf5s[1] = hdf5s[1], hdf5s[0]
+		for hdf5 in hdf5s:
+			flags = utils.pkgconfig.parse(conf, hdf5)
+			if flags:
+				break
 
-
-	# Parse the output from the h5cc compiler wrapper
-	def parse_func(env, cmd):
-		# remove the compiler
-		cmd = cmd.partition(' ')[2]
-		# remove unknown arguments
-		cmd = utils.compiler.removeUnknownOptions(cmd)
-		return env.ParseFlags(cmd)
-	flags = env.ParseConfig([h5cc, '-show', '-shlib'], parse_func)
+		if not flags:
+			if required:
+				print 'Could not find h5cc or h5pcc. Make sure the path to the HDF5 library is correct!'
+				env.Exit(1)
+			else:
+				conf.Finish()
+				return False
 
 	utils.pkgconfig.appendPathes(env, flags)
 
@@ -80,6 +107,22 @@ def find(env, required=True, parallel=False, hl=True):
 		else:
 			conf.Finish()
 			return False
+
+	if api:
+		ret = conf.CheckAPIVersion(16)
+		if api == 18 and ret:
+			# Redefine all macros
+			conf.env.Append(CPPDEFINES=['H5Dcreate_vers=2',
+				'H5Dopen_vers=2',
+				'H5Gcreate_vers=2',
+				'H5Gopen_vers=2',
+				'H5Acreate_vers=2',
+				'H5Eget_auto_vers=2',
+				'H5Eset_auto_vers=2'])
+			# TODO add more
+		elif api == 16 and not ret:
+			# Force 16 API
+			conf.env.Append(CPPDEFINES=['H5_USE_16_API'])
 
 	conf.Finish()
 	return True
