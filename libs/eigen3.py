adb17e53892d72a06c265e2d8fd1473b2d886c24
Carsten Uphoff <uphoff@in.tum.de>
2019-04-10 20:25:42 +0200

    Added eigen3

---
+++ libs/eigen3.py
@@ -0,0 +1,59 @@
+#! /usr/bin/python
+
+## @file
+# This file is part of scons-tools.
+#
+# @author Carsten Uphoff <c.uphoff@tum.de>
+#
+# @copyright Copyright (c) 2019, Technische Universitaet Muenchen.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import utils.checks
+import utils.pkgconfig
+
+def find(env, required=True):
+  conf = env.Configure()
+  utils.checks.addDefaultTests(conf)
+
+  flags = utils.pkgconfig.parse(conf, 'eigen3', ['--cflags'])
+
+  utils.pkgconfig.appendPathes(env, flags)
+
+  if not conf.CheckHeader('Eigen/Eigen', language='c++'):
+    if required:
+      utils.checks.error('Could not find eigen3')
+      env.Exit(1)
+    else:
+      conf.Finish()
+      return False
+
+  env.Append(CPPDEFINES=['-DHAS_EIGEN3'])
+
+  conf.Finish()
+  return True
